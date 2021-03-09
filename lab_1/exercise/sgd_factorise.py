cc27f211a132af64426af1edf91ef139f038916f
Eugene Teoh <eugenetwc1@gmail.com>
2021-03-09 11:34:23 +0000

    completed lab 1 exercise; TODO: lab 1 report

---
+++ lab_1/exercise/sgd_factorise.py
@@ -0,0 +1,34 @@
+from typing import Tuple
+import torch
+from torch.nn.functional import mse_loss
+
+def sgd_factorise(A: torch.Tensor, rank: int, num_epochs=1000, lr=0.01) -> Tuple[torch.Tensor, torch.Tensor]:
+    m, n = A.shape
+    U = torch.rand((m, rank))
+    V = torch.rand((n, rank)) 
+
+    for epoch in range(num_epochs):
+        for r in range(m):
+            for c in range(n):
+                e = A[r, c] - (U[r] @ V[c].T)
+                U[r] += lr * e * V[c]
+                V[c] += lr * e * U[r]
+
+    return U, V
+
+def sgd_factorise_masked(A: torch.Tensor, M: torch.Tensor, rank: int, num_epochs=1000, lr=0.01) -> Tuple[torch.Tensor, torch.Tensor]:
+    m, n = A.shape
+    U = torch.rand((m, rank))
+    V = torch.rand((n, rank)) 
+
+    A = A * M # masked A
+
+    for epoch in range(num_epochs):
+        for r in range(m):
+            for c in range(n):
+                if A[r, c]:
+                    e = A[r, c] - (U[r] @ V[c].T)
+                    U[r] += lr * e * V[c]
+                    V[c] += lr * e * U[r]
+
+    return U, V
